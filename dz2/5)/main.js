0cffa81eefa8beb860e0160c38a44ef286815027
Aleksandr Dvortsov <sashadvortsov1@gmail.com>
2019-07-04 15:32:07 +0300

    all done

--- dz2/5)/main.js
+++ dz2/5)/main.js
@@ -3,8 +3,7 @@ var arrCitys = ['Kiev', 'Lviv', 'Odessa', 'Kharkiv', 'Dnepr'];
 var arr1 = arrCitys;
 var arr2 = [];
 
-function createLiCity1(arr) {
-
+function createLiCity(arr, count) {
     var list = document.createElement('ul');
 
     for(var i = 0; i < arr.length; i++) {
@@ -23,54 +22,35 @@ function createLiCity1(arr) {
         
         li.appendChild(btn);
         btn.addEventListener('click',function(e){
+            if (count === 1){
+                var index = arr1.indexOf(e.target.parentElement.firstChild.innerHTML);
+                if (index >= 0) {
+                    arr1.splice( index, 1 );
+                }
 
-            var index = arr1.indexOf(e.target.parentElement.firstChild.innerHTML);
-            if (index >= 0) {
-                arr1.splice( index, 1 );
+                arr2.push(e.target.parentElement.firstChild.innerHTML);
+                update();
             }
+            if (count === 2) {
+                var index = arr2.indexOf(e.target.parentElement.firstChild.innerHTML);
+                if (index >= 0) {
+                    arr2.splice( index, 1 );
+                }
 
-            arr2.push(e.target.parentElement.firstChild.innerHTML);
-            update();
+                arr1.push(e.target.parentElement.firstChild.innerHTML);
+                update();
+            }
          });
         list.appendChild(li);
     }
-    document.getElementById("city1").appendChild(list);
-}
-
-
-function createLiCity2(arr) {
-
-    var list = document.createElement('ul');
-
-    for(var i = 0; i < arr.length; i++) {
-
-        var li = document.createElement('li');
-        li.style.alignItems = 'center';
-
-        var p = document.createElement('h5');
-        li.appendChild(p);
-
-        p.appendChild(document.createTextNode(arr[i]));
-        
-        var btn = document.createElement('button');
-        btn.innerText = 'x';
-        btn.style.height = '20px';
-        
-        li.appendChild(btn);
-        btn.addEventListener('click',function(e){
-
-            var index = arr2.indexOf(e.target.parentElement.firstChild.innerHTML);
-            if (index >= 0) {
-                arr2.splice( index, 1 );
-            }
-
-            arr1.push(e.target.parentElement.firstChild.innerHTML);
-            update();
-         });
-        list.appendChild(li);
+    if (count === 1) {
+        document.getElementById("city1").appendChild(list);
     }
-    document.getElementById("city2").appendChild(list);
+    if (count === 2) {
+        document.getElementById("city2").appendChild(list);
+    }
 }
+
 
 function update() {
     
@@ -82,8 +62,9 @@ function update() {
     while (divCity2.firstChild) {
         divCity2.removeChild(divCity2.firstChild);
     }
-    createLiCity1(arr1);
-    createLiCity2(arr2);
+    createLiCity(arr1, 1);
+    createLiCity(arr2, 2);
 }
-createLiCity1(arr1);
-createLiCity2(arr2);
+
+createLiCity(arr1, 1);
+createLiCity(arr2, 2);
