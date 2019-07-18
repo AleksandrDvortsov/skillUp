b54396032ffa567b8f94cfa7d3c8c1cc7bee4e22
skillup <sashadvortsov1@gmail.com>
2019-07-18 21:35:59 +0300

    destructurung end

--- cw/18.07.19 (destructuring)/main.js
+++ cw/18.07.19 (destructuring)/main.js
@@ -1,4 +1,18 @@
+// ca5a2a324ba06f2cf8bede88a989bb6c2f5f87730032b3c6256b72888f2cc94c
 
+
+const user = {
+    name: "vasa",
+    age: 25,
+    email: 'xz@gmail.com'
+}
+
+function rendom({
+    name, email}) {
+    console.log(age);
+}
+
+rendom(user);
 
 
 
