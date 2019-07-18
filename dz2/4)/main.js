1b85986ed48f67a72aab614ce9b036314f758a98
skillup <sashadvortsov1@gmail.com>
2019-07-18 20:09:39 +0300

    fix dz4

--- dz2/4)/main.js
+++ dz2/4)/main.js
@@ -1,18 +1,64 @@
-
+let countT = 155;
+let countB = 155;
+let countL = 483;
+let countR = 483;
 
 function topClick() {
-    document.getElementById('cont').style.alignItems  = "flex-start";
+    let top = setInterval(function() {
+        if(document.getElementById('cont').style.paddingTop === '0px'){
+            clearTimeout(top);
+            return;
+        }
+       
+        countT -= 5;
+        countB += 5;
+        document.getElementById('cont').style.paddingTop = countT + 'px';
+        document.getElementById('cont').style.paddingBottom = countB + 'px';
+
+    },100);
 }
 function bottomClick() {
-    document.getElementById('cont').style.alignItems  = "flex-end";
+    let bottom = setInterval(function() {
+        if(document.getElementById('cont').style.paddingBottom === '0px'){
+            clearTimeout(bottom);
+            return;
+        }
+       
+        countB -= 5;
+        countT += 5;
+        document.getElementById('cont').style.paddingTop = countT + 'px';
+        document.getElementById('cont').style.paddingBottom = countB + 'px';
+
+    },100);
 }
+
 function leftClick() {
-    document.getElementById('cont').style.justifyContent   = "flex-start";
+    let left = setInterval(function() {
+        if(document.getElementById('cont').style.paddingLeft === '3px'){
+            clearTimeout(left);
+            return;
+        }
+       
+        countL -= 5;
+        countR += 5;
+        document.getElementById('cont').style.paddingLeft = countL + 'px';
+        document.getElementById('cont').style.paddingRight = countR + 'px';
+
+    },100);
 }
+
 function rightClick() {
-    document.getElementById('cont').style.justifyContent  = "flex-end";
+    let right = setInterval(function() {
+        if(document.getElementById('cont').style.paddingLeft === '1083px'){
+            clearTimeout(right);
+            return;
+        }
+       
+        countR -= 5;
+        countL += 5;
+        document.getElementById('cont').style.paddingLeft = countL + 'px';
+        document.getElementById('cont').style.paddingRight = countR + 'px';
+
+    },100);
 }
-function centrClick() {
-    document.getElementById('cont').style.alignItems  = "center";
-    document.getElementById('cont').style.justifyContent  = "center";
-}
+
