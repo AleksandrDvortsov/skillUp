d70838ae6b6c4167cc0cceaefc594df91a6a89ec
Aleksandr Dvortsov <sashadvortsov1@gmail.com>
2019-07-20 12:17:27 +0300

    first assignment redone for OOP

--- dz2/1)/main.js
+++ dz2/1)/main.js
@@ -1,21 +1,35 @@
 var arr = ['Vasia', 'Petia', 'Alex', 'Ivan'];
+var arr2 = ['Vasia2', 'Petia2', 'Alex2', 'Ivan2'];
 
-function createPerson() {
-
-    var list = document.createElement('ul');
-
-    for(var i = 0; i < arr.length; i++) {
-
-        var li = document.createElement('li');
-
-        li.appendChild(document.createTextNode(arr[i]));
-        li.addEventListener('click',function(){
-            alert(this.innerHTML )
-         });
-
-        list.appendChild(li);
+function CreatePerson(arr, id) {
+    this.ar = arr;
+    
+    this.init = function() {
+        this.list = document.createElement('ul');
+        this.create();
+        this.render();
     }
-    document.getElementById("person").appendChild(list);
+
+    this.create = function() {
+        for(let i = 0; i < this.ar.length; i++) {
+
+            let li = document.createElement('li');
+    
+            li.appendChild(document.createTextNode(this.ar[i]));
+             li.onclick = function () {
+                alert(this.innerHTML );
+             }
+             this.list.appendChild(li);
+        }
+    }
+
+    this.render = function() {
+        document.querySelector(id).appendChild(this.list);
+    }
+
+    this.init();
 }
 
-createPerson();
+let newCreate = new CreatePerson(arr, "#person");
+let newCreate2 = new CreatePerson(arr2, "#person2");
+
