46e7e066bfd99c8f3ea16b7cb78bb7f9e130dd44
Aleksandr Dvortsov <sashadvortsov1@gmail.com>
2019-07-20 13:06:22 +0300

    second assignment redone for OOP

--- dz2/2)/main.js
+++ dz2/2)/main.js
@@ -1,43 +1,66 @@
-var arrPerson = [];
-var count = 0;
 
-function input() {
-    var name = document.getElementById('namePerson').value;
-    
-    if(name){
-        arrPerson.push(name);
-        createPerson(count);
-        count++;
+function CreatePerson(blockNav, id) {
+  this.init = function() {
+    this.arrPerson = [];
+    this.count = 0;
+    this.id = document.querySelector(id);
+
+    this.nav = document.querySelector(blockNav);
+    this.inputText = document.createElement("input");
+    this.inputText.type = "text";
+    this.inputText.id = "textInput";
+
+    this.btnInput = document.createElement("button");
+    this.btnInput.type = "button";
+    this.btnInput.innerHTML = "Input";
+    this.btnInput.onclick = this.inputFun;
+
+    this.btnDel = document.createElement("button");
+    this.btnDel.type = "button";
+    this.btnDel.innerHTML = "Delete";
+    this.btnDel.onclick = this.deleteFun;
+
+    this.nav.appendChild(this.inputText);
+    this.nav.appendChild(this.btnInput);
+    this.nav.appendChild(this.btnDel);
+  };
+
+  this.inputFun = function() {
+    this.name = this.inputText.value;
+
+    if (this.name) {
+      this.arrPerson.push(this.name);
+      this.create();
+      this.count++;
+    } else {
+        alert('print name!')
     }
+  }.bind(this);
+
+  this.deleteFun = function() {
+    while (this.id.firstChild) {
+        this.id.removeChild(this.id.firstChild);
+    }
+
+    this.arrPerson.length = 0;
+    this.count = 0;
+  }.bind(this);
+
+  this.create = function() {
+    this.list = document.createElement("ul");
+    this.li = document.createElement("li");
+    this.li.appendChild(document.createTextNode(this.arrPerson[this.count]));
+    this.li.onclick = function(e) {
+      alert(e.target.innerHTML);
+    };
+
+    this.list.appendChild(this.li);
+    this.id.appendChild(this.list);
+  }.bind(this);
+
+  this.init();
 }
 
-function del() {
+let block = new CreatePerson("#nav", "#person");
 
-    var divPerson = document.getElementById("person");
-    while (divPerson.firstChild) {
-        divPerson.removeChild(divPerson.firstChild);
-    }
-
-    arrPerson.length = 0;
-    count = 0;
-}
-
-function createPerson(count) {
-
-    var list = document.createElement('ul');
-
-    var li = document.createElement('li');
-
-    li.appendChild(document.createTextNode(arrPerson[count]));
-    li.onclick = function (e) {
-        alert(e.target.innerHTML)
-    }
-    // li.addEventListener('click',function(e){
-    //     alert(e.target.innerHTML)
-    // });
-
-    list.appendChild(li);
-    document.getElementById("person").appendChild(list);
-}
-
-
+let block2 = new CreatePerson("#nav2", "#person2");
