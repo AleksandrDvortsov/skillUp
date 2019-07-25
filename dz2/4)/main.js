ab6b1e359bdc73a1cf08c6478fc8d26ff91d6803
Aleksandr Dvortsov <sashadvortsov1@gmail.com>
2019-07-25 16:54:32 +0300

    the fourth task is redone for OOP

--- dz2/4)/main.js
+++ dz2/4)/main.js
@@ -1,97 +1,81 @@
-let countT = 155;
-let countB = 155;
-let countL = 483;
-let countR = 483;
+function Animation() {
+  this.init = () => {
+    this.countT = 155;
+    this.countB = 155;
+    this.countL = 575;
+    this.countR = 575;
 
-function topClick() {
-    let top = setInterval(function() {
-        if(document.getElementById('cont').style.paddingTop === '0px'){
-            clearTimeout(top);
+    this.btnT = document.getElementById("btnT");
+    this.btnT.onclick = this.top;
+    this.btnB = document.getElementById("btnB");
+    this.btnB.onclick = this.bottom;
+
+    this.btnR = document.getElementById("btnR");
+    this.btnR.onclick = this.right;
+    this.btnL = document.getElementById("btnL");
+    this.btnL.onclick = this.left;
+  };
+
+  this.top = () => {
+    this.redraw("top");
+  };
+  this.bottom = () => {
+    this.redraw("bottom");
+  };
+
+  this.left = () => {
+    this.redraw("left");
+  };
+  this.right = () => {
+    this.redraw("right");
+  };
+
+  this.redraw = function(type) {
+    let draw = setInterval(() => {
+      switch (type) {
+        case "top":
+          if (document.getElementById("cont").style.paddingTop === "0px") {
+            clearTimeout(draw);
             return;
-        }
-       
-        countT -= 5;
-        countB += 5;
-        document.getElementById('cont').style.paddingTop = countT + 'px';
-        document.getElementById('cont').style.paddingBottom = countB + 'px';
-
-    },100);
-}
-function bottomClick() {
-    let bottom = setInterval(function() {
-        if(document.getElementById('cont').style.paddingBottom === '0px'){
-            clearTimeout(bottom);
+          }
+          this.countT -= 5;
+          this.countB += 5;
+          break;
+        case "bottom":
+          if (document.getElementById("cont").style.paddingBottom === "0px") {
+            clearTimeout(draw);
             return;
-        }
-       
-        countB -= 5;
-        countT += 5;
-        document.getElementById('cont').style.paddingTop = countT + 'px';
-        document.getElementById('cont').style.paddingBottom = countB + 'px';
-
-    },100);
-}
-
-function leftClick() {
-    let left = setInterval(function() {
-        if(document.getElementById('cont').style.paddingLeft === '3px'){
-            clearTimeout(left);
+          }
+          this.countB -= 5;
+          this.countT += 5;
+          break;
+        case "right":
+          let num = parseInt(document.getElementById("cont").style.paddingLeft);
+          if (num >= window.innerWidth - 220 && num != NaN) {
+            clearTimeout(draw);
             return;
-        }
-       
-        countL -= 5;
-        countR += 5;
-        document.getElementById('cont').style.paddingLeft = countL + 'px';
-        document.getElementById('cont').style.paddingRight = countR + 'px';
-
-    },100);
-}
-
-function rightClick() {
-    let right = setInterval(function() {
-        if(document.getElementById('cont').style.paddingLeft === '1083px'){
-            clearTimeout(right);
+          }
+          this.countR -= 5;
+          this.countL += 5;
+          break;
+        case "left":
+          if (document.getElementById("cont").style.paddingLeft === "0px") {
+            clearTimeout(draw);
             return;
-        }
-       
-        countR -= 5;
-        countL += 5;
-        document.getElementById('cont').style.paddingLeft = countL + 'px';
-        document.getElementById('cont').style.paddingRight = countR + 'px';
+          }
+          this.countR += 5;
+          this.countL -= 5;
+          break;
+      }
 
-    },100);
+      document.getElementById("cont").style.paddingTop = this.countT + "px";
+      document.getElementById("cont").style.paddingBottom = this.countB + "px";
+
+      document.getElementById("cont").style.paddingLeft = this.countL + "px";
+      document.getElementById("cont").style.paddingRight = this.countR + "px";
+    }, 10);
+  };
+
+  this.init();
 }
-
-
-
-
-function XXX () {
-   
-    this.init = function() {
-        this.btn = document.getElementById('btnR');
-        this.btn.onclick = this.redraw;
-    }
-    // console.log(this.btn);
-    this.right = function() {
-        this.redraw;
-        // this.redraw(document.getElementById('cont').style.paddingLeft, '1083px',countR,countL,document.getElementById('cont').style.paddingLeft,document.getElementById('cont').style.paddingRight = countR);
-    }
-    this.redraw = function(if1, if2, count1, count2, el1, el2) {
-        alert('33')
-        let xz = setInterval(function() {
-            console.log(this);
-            if(if1 === if2){
-                clearTimeout(xz);
-                return;
-            }
-            count1 -= 5;
-            count2 += 5;
-            el1 = countL + 'px';
-            el2 = countR + 'px';
-        }.bind(this),100)
-    }
-    
-    this.init();
-}
-
-let x = new XXX();
+let animation = new Animation();
